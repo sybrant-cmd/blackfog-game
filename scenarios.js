e1e8ab8e7565edd6345084fc0186575f46a731b3
sybrant-cmd <slzen@pm.me>
2025-11-26 09:13:36 +0800

    Delete scenarios.js

--- scenarios.js
+++
@@ -1,8 +0,0 @@
-// scenarios.js
-export const scenarios = {
-  sc01_mft_zero_day: { title: "MFT Zero-Day Exploit", injects: [ {seq:1, narrative:"Sample"} ] },
-  sc02_idp_support_breach: { title: "IdP Support Breach", injects: [] },
-  sc03_casino_ransom: { title: "Casino Ransomware", injects: [] },
-  sc04_vendor_ransom: { title: "Vendor Ransom Impact", injects: [] },
-  sc05_cloud_misconfig: { title: "Cloud Misconfiguration", injects: [] }
-};
